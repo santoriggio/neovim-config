03f1be4b3ac5ac0ff5bb89ca4860bc952bf11ace
Santo <info@santoriggio.it>
2024-06-07 19:33:20 +0200

    feat: add autoclose tag
    add autoclose tag plugin

--- lua/plugins/autoclose.lua
+++ lua/plugins/autoclose.lua
@@ -1,6 +1,14 @@
 return {
-  "m4xshen/autoclose.nvim",
-  config = function()
-    require("autoclose").setup()
-  end,
+	{
+		"windwp/nvim-ts-autotag",
+		config = function()
+			require("nvim-ts-autotag").setup()
+		end,
+	},
+	{
+		"m4xshen/autoclose.nvim",
+		config = function()
+			require("autoclose").setup()
+		end,
+	},
 }
