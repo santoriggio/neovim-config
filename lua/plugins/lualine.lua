af8689a64c17f51d7f02eec840c1a84e9b5b04ae
Santo Riggio <s.riggio@be-tse.it>
2024-08-03 10:02:40 +0200

    feat: lualine setup

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,18 +2,6 @@ return {
 	"nvim-lualine/lualine.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
-		require("lualine").setup({
-			sections = {
-				lualine_c = {
-					{
-						"filename",
-						path = 1,
-					},
-				},
-				lualine_x = {
-					{},
-				},
-			},
-		})
+		require("lualine").setup()
 	end,
 }
