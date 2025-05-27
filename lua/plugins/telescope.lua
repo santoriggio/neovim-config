699471cd91e41652932d83429c3f8100f4bdc77e
Santo Riggio <info@santoriggio.it>
2025-05-27 14:59:28 +0200

    fix: add config to ensure performance on live grep

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,34 +1,103 @@
 return {
-	{
-		"nvim-telescope/telescope.nvim",
-		tag = "0.1.6",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		config = function()
-			require("telescope").setup({
-				defaults = {
-					file_ignore_patterns = {
-						"node_modules",
-						"ios",
-						"android",
-					},
-				},
-			})
-			local builtin = require("telescope.builtin")
-			vim.keymap.set("n", "<C-p>", builtin.find_files, {})
-			vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-		end,
-	},
-	{
-		"nvim-telescope/telescope-ui-select.nvim",
-		config = function()
-			require("telescope").setup({
-				extensions = {
-					["ui-select"] = {
-						require("telescope.themes").get_dropdown({}),
-					},
-				},
-			})
-			require("telescope").load_extension("ui-select")
-		end,
-	},
+  {
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.6",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("telescope").setup({
+        defaults = {
+          file_ignore_patterns = {
+            "node_modules",
+            "ios/Pods",
+            "ios/build",
+            "android/.gradle",
+            "android/app/build",
+            "android/build",
+            "map-bundles",
+          },
+          vimgrep_arguments = {
+            "rg",
+            "--color=never",
+            "--no-heading",
+            "--with-filename",
+            "--line-number",
+            "--column",
+            "--smart-case",
+            "--max-depth=4",
+            "--max-filesize=1M",
+            "--hidden",
+          },
+          prompt_prefix = "🔍 ",
+          selection_caret = "❯ ",
+          entry_prefix = "  ",
+          initial_mode = "insert",
+          selection_strategy = "reset",
+          sorting_strategy = "descending",
+          layout_strategy = "horizontal",
+          layout_config = {
+            horizontal = {
+              mirror = false,
+              preview_width = 0.5,
+            },
+            vertical = {
+              mirror = false,
+            },
+          },
+          file_sorter = require("telescope.sorters").get_fuzzy_file,
+          generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
+          path_display = { "truncate" },
+          winblend = 0,
+          border = {},
+          borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+          color_devicons = true,
+          use_less = true,
+          set_env = { ["COLORTERM"] = "truecolor" },
+          file_previewer = require("telescope.previewers").vim_buffer_cat.new,
+          grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
+          qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
+          buffer_previewer_maker = require("telescope.previewers").buffer_previewer_maker,
+          mappings = {
+            i = {
+              ["<C-n>"] = "move_selection_next",
+              ["<C-p>"] = "move_selection_previous",
+              ["<C-c>"] = "close",
+              ["<C-j>"] = "preview_scrolling_down",
+              ["<C-k>"] = "preview_scrolling_up",
+              ["<C-q>"] = "send_to_qflist",
+              ["<CR>"] = "select_default",
+            },
+            n = {
+              ["<C-c>"] = "close",
+              ["<C-q>"] = "send_to_qflist",
+              ["<C-n>"] = "move_selection_next",
+              ["<C-p>"] = "move_selection_previous",
+              ["<CR>"] = "select_default",
+            },
+          },
+        },
+        pickers = {
+          live_grep = {
+            only_sort_text = true,
+            previewer = false,
+          },
+        },
+      })
+      local builtin = require("telescope.builtin")
+      vim.keymap.set("n", "<C-p>", builtin.find_files, {})
+      vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+    end,
+  },
+  {
+    "nvim-telescope/telescope-ui-select.nvim",
+    config = function()
+      require("telescope").setup({
+        extensions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown({}),
+          },
+        },
+      })
+      require("telescope").load_extension("ui-select")
+    end,
+  },
 }
