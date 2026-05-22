bf9433a3121b4509ae67376d03de8d330b40473f
Santo Riggio <info@santoriggio.it>
2026-05-22 15:03:59 +0200

    fix: update

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -7,42 +7,40 @@ return {
   },
   {
     "williamboman/mason-lspconfig.nvim",
+    dependencies = { "williamboman/mason.nvim" },
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "ts_ls", "cssls", "rust_analyzer", "arduino_language_server", "eslint" },
+        ensure_installed = { "lua_ls", "ts_ls", "cssls", "rust_analyzer", "arduino_language_server" },
+        automatic_enable = false,
       })
     end,
   },
   {
     "neovim/nvim-lspconfig",
+    dependencies = { "williamboman/mason-lspconfig.nvim" },
     config = function()
-      local lsp = require("lspconfig")
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      lsp.lua_ls.setup({
+      vim.lsp.config("lua_ls", {
         capabilities = capabilities,
       })
 
-      lsp.ts_ls.setup({
+      vim.lsp.config("ts_ls", {
         capabilities = capabilities,
-        on_attach = function(client, bufnr)
-          -- Disabilita formattazione se hai altri formatter
+        on_attach = function(client, _bufnr)
           client.server_capabilities.documentFormattingProvider = false
           client.server_capabilities.documentRangeFormattingProvider = false
         end,
         settings = {
           typescript = {
-            -- Abilita la risoluzione automatica degli alias
             preferences = {
               includePackageJsonAutoImports = "auto",
               importModuleSpecifierPreference = "relative",
             },
-            -- Configurazione per la risoluzione dei path
             suggest = {
               autoImports = true,
               includeCompletionsForImportStatements = true,
             },
-            -- Abilita la risoluzione dei path dal tsconfig.json
             inlayHints = {
               parameterNames = { enabled = "all" },
               variableTypes = { enabled = true },
@@ -62,45 +60,36 @@ return {
         },
       })
 
-      lsp.cssls.setup({
+      vim.lsp.config("cssls", {
         capabilities = capabilities,
       })
 
-      lsp.rust_analyzer.setup({
+      vim.lsp.config("rust_analyzer", {
         capabilities = capabilities,
       })
 
-      lsp.eslint.setup({
-        capabilities = capabilities,
-        on_attach = function(client, bufnr)
-          -- ESLint può formattare, ma di solito si preferisce Prettier
-          client.server_capabilities.documentFormattingProvider = false
-        end,
-        settings = {
-          -- Configurazione per la risoluzione degli alias
-          workingDirectory = { mode = "auto" },
-          codeAction = {
-            disableRuleComment = {
-              enable = true,
-              location = "separateLine"
-            }
-          },
-          format = false, -- Usa Prettier per il formatting
-        },
-      })
-
-      local MY_FQBN = "arduino:avr:uno"
-      local ESP8266_FQBN = "esp8266:esp8266:nodemcu"
       local HOME = vim.fn.expand("~")
-      lsp.arduino_language_server.setup({
+      local MY_FQBN = "arduino:avr:uno"
+      vim.lsp.config("arduino_language_server", {
         capabilities = capabilities,
         cmd = {
           "arduino-language-server",
-          "-cli-config", HOME .. "/Library/Arduino15/arduino-cli.yaml",
+          "-cli-config",
+          HOME .. "/Library/Arduino15/arduino-cli.yaml",
           "-fqbn",
           MY_FQBN,
-        }
+        },
       })
+
+      for _, server in ipairs({
+        "lua_ls",
+        "ts_ls",
+        "cssls",
+        "rust_analyzer",
+        "arduino_language_server",
+      }) do
+        vim.lsp.enable(server)
+      end
     end,
   },
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,8 +1,9 @@
 -- Need to install fd, ripgrep
 -- brew install fd ripgrep
+-- find_files: sorter nativo (fuzzy meno permissivo); live_grep: fzf-native
 return {
   "nvim-telescope/telescope.nvim",
-  tag = "0.1.6",
+  tag = "v0.2.1",
   dependencies = {
     "nvim-lua/plenary.nvim",
     {
@@ -28,7 +29,7 @@ return {
           "--glob=!.git/*",
           "--glob=!node_modules/*",
           "--glob=!*.lock",
-          "--max-filesize=1M"
+          "--max-filesize=1M",
         },
         prompt_prefix = " ",
         selection_caret = " ",
@@ -40,7 +41,7 @@ return {
         layout_config = {
           horizontal = {
             mirror = false,
-            preview_width = 0.5,
+            preview_width = 0.55,
           },
           vertical = {
             mirror = false,
@@ -48,18 +49,17 @@ return {
         },
         file_sorter = require("telescope.sorters").get_fuzzy_file,
         generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
-        path_display = { "truncate" },
+        path_display = { "filename_first" },
         winblend = 0,
         border = {},
         borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
         color_devicons = true,
-        use_less = true,
+        use_less = false,
         set_env = { ["COLORTERM"] = "truecolor" },
         file_previewer = require("telescope.previewers").vim_buffer_cat.new,
         grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
         qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
         buffer_previewer_maker = require("telescope.previewers").buffer_previewer_maker,
-        -- Aggiungi cache per performance
         cache_index = true,
         mappings = {
           i = {
@@ -82,25 +82,17 @@ return {
       },
       pickers = {
         find_files = {
-          find_command = {
-            "fd",
-            "--type", "f",
-            "--strip-cwd-prefix",
-            "--hidden",
-            "--exclude", ".git",
-            "--exclude", "node_modules"
-          },
+          respect_gitignore = true,
         },
         live_grep = {
           only_sort_text = true,
-          previewer = true
         },
       },
       extensions = {
         fzf = {
           fuzzy = true,
           override_generic_sorter = true,
-          override_file_sorter = true,
+          override_file_sorter = false,
           case_mode = "smart_case",
         },
       },
@@ -108,35 +100,52 @@ return {
 
     local builtin = require("telescope.builtin")
 
-    -- Funzione per live grep con filtro per estensioni
+    local function fd_find_command(hidden)
+      local cmd = {
+        "fd",
+        "--type", "f",
+        "--strip-cwd-prefix",
+        "--exclude", ".git",
+        "--exclude", "node_modules",
+      }
+      if hidden then
+        table.insert(cmd, "--hidden")
+      end
+      return cmd
+    end
+
+    local function find_files(hidden)
+      builtin.find_files({
+        respect_gitignore = true,
+        find_command = fd_find_command(hidden),
+      })
+    end
+
     local function live_grep_by_extension()
       local extension = vim.fn.input("Estensione file (es: .js, .py, .lua): ")
       if extension ~= "" then
-        -- Rimuovi il punto se presente all'inizio
         extension = extension:gsub("^%.", "")
         builtin.live_grep({
           additional_args = function()
             return { "--type-add", "custom:*." .. extension, "--type", "custom" }
-          end
+          end,
         })
       else
         builtin.live_grep()
       end
     end
 
-    -- Funzione per live grep in una cartella specifica
     local function live_grep_in_folder()
       local folder = vim.fn.input("Cartella (es: src/, lua/, .): ")
       if folder ~= "" then
         builtin.live_grep({
-          search_dirs = { folder }
+          search_dirs = { folder },
         })
       else
         builtin.live_grep()
       end
     end
 
-    -- Funzione per live grep con esclusione di cartelle
     local function live_grep_exclude_folders()
       local exclude_folders = vim.fn.input("Cartelle da escludere (es: node_modules,dist): ")
       if exclude_folders ~= "" then
@@ -148,38 +157,36 @@ return {
         builtin.live_grep({
           additional_args = function()
             return folders
-          end
+          end,
         })
       else
         builtin.live_grep()
       end
     end
 
-    -- Funzione per live grep con pattern personalizzato
     local function live_grep_with_pattern()
       local pattern = vim.fn.input("Pattern file (es: *.{js,ts}, **/*.py): ")
       if pattern ~= "" then
         builtin.live_grep({
           additional_args = function()
             return { "--glob", pattern }
-          end
+          end,
         })
       else
         builtin.live_grep()
       end
     end
 
-    -- Keymaps standard
-    vim.keymap.set("n", "<C-p>", builtin.find_files, {})
-    vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-    vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+    vim.keymap.set("n", "<C-p>", function() find_files(false) end, { desc = "Find files" })
+    vim.keymap.set("n", "<leader>ff", function() find_files(false) end, { desc = "Find files" })
+    vim.keymap.set("n", "<leader>fH", function() find_files(true) end, { desc = "Find files (hidden)" })
+    vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live grep" })
     vim.keymap.set("n", "<leader>fw", function()
       builtin.grep_string({ search = vim.fn.input("Grep > ") })
-    end)
-    vim.keymap.set("n", "<leader>m", builtin.marks, {})
-    vim.keymap.set("n", "<leader>fq", builtin.quickfix, {})
+    end, { desc = "Grep string" })
+    vim.keymap.set("n", "<leader>m", builtin.marks, { desc = "Marks" })
+    vim.keymap.set("n", "<leader>fq", builtin.quickfix, { desc = "Quickfix" })
 
-    -- Nuovi keymaps per le funzionalità avanzate di live grep
     -- vim.keymap.set("n", "<leader>fge", live_grep_by_extension, { desc = "Live grep per estensione" })
     -- vim.keymap.set("n", "<leader>fgf", live_grep_in_folder, { desc = "Live grep in cartella" })
     -- vim.keymap.set("n", "<leader>fgx", live_grep_exclude_folders, { desc = "Live grep escludendo cartelle" })
